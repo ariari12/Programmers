159efd5459bda329b3ee5856b9aedf2def44df0e
ariari12 <66054061+ariari12@users.noreply.github.com>
2024-02-14 22:55:34 +0900

    [level 0] Title: 문자열 섞기, Time: 1.30 ms, Memory: 71.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String str1, String str2) {
+        String answer = "";
+        char[] ch1 =str1.toCharArray();
+        char[] ch2 =str2.toCharArray();
+        for(int i=0; i<ch1.length; i++){
+            answer+=String.valueOf(ch1[i]);
+            answer+=String.valueOf(ch2[i]);
+        }
+
+        return answer;
+    }
+}
