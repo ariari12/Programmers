20cf641a58b332e4e44d8d5da54ba711fd32e080
KAH <66054061+ariari12@users.noreply.github.com>
2025-01-30 23:02:23 +0900

    [level 2] Title: 올바른 괄호, Time: 14.74 ms, Memory: 54.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    boolean solution(String s) {
+        
+        List<Character> stack = new ArrayList<>();
+        for(char c : s.toCharArray()){
+            if(c == '('){
+                stack.add(c);
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }else{
+                    stack.remove(stack.size() - 1);
+                }
+            }
+        }
+        return stack.isEmpty();
+
+        
+    }
+}
